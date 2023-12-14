32e177661db4300de0451be571e9a6d3f378c263
SvenK <git+sveto@svenk.org>
2023-12-14 01:56:14 +0100

    Tested most C++ features. Works.
    VFTs, global symbols, cons and destructors, initialization, heap.

--- test-module/payload.cpp
+++ test-module/payload.cpp
@@ -1,36 +1,57 @@
 #include <Arduino.h>
 #include <cstdint>
 
-extern "C" { // for startup symbol
-  
-/*
-void test_main() {
-  Serial.println("Hello World.");
-}
-*/
+//#include "new.cpp"
 
+// Weird: Why is this needed?
+void * operator new(size_t size) { return malloc(size); }
+
+struct Test {
+  int a;
+  
+  virtual void f() { Serial.printf("Test::f, a=%d\n",a );  }
+  
+  Test(int _a) : a(_a) { Serial.printf("Test::Test(%d)\n", _a); }
+  void dump() { Serial.printf("Test::bar() prints a=%d\n",a); }
+  ~Test() { Serial.printf("Test::Test~(a was %d)\n", a); }
+};
+
+struct Hansi : public Test {
+  Hansi(int a) : Test(a) { Serial.println("Hansi::Hansi"); }
+  void f() override { Serial.printf("Hansi::f, a=%d\n", a); }
+};
+
+int  x = 17;
+Test *h;
+const char* z = "Hallo Welt\n";
+
+
+extern "C" { // for startup symbol
 void call_me_back(); // Defined in main program
+}
 
 uint8_t adder(uint8_t a, uint8_t b) {
   return a+b;
 }
 
-void any_function() {
-  call_me_back();
+void somewhere() {
+  Serial.println("Somewhere starting");
+  Test t(456);
+  t.f();
+  Serial.println("Somewhere finishing");
 }
 
-void third_function() {
+void test_main_actual() {
+  Serial.println("test_main starting");
+  Serial.printf("global *x=%x, value  x=%d\n", &x, x);
+  Serial.print(z);
   call_me_back();
-}
-
-void test_main() {
-  any_function();
+  somewhere();
+  h = new Hansi(77);
+  h->f();
   Serial.println("test_main");
 }
 
-void fourth_function() {
-  any_function();
-}
-
-
+extern "C" { // for startup symbol
+  void test_main() { test_main_actual(); }
 }
